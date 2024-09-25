1f53f9cf5d8b9dac155d1eea3598d02f6ec08420
Stormbreaker20 <luvsharma.del@gmail.com>
2024-09-25 14:38:23 +0530

    Amazon ML Challenge

---
+++ AmazonML/Dataset_Resources-20240912T185403Z-001/Dataset_Resources/src/temp.py
@@ -0,0 +1,69 @@
+
+
+
+Extracted Text:
+5S Stee OT eae Ter SEEPS Ce oe eer or SS a ste ==
+[Skye oR S St et SS ee See Tan GR
+cdatare Wi lS ut CUR Ris eS Ry eee Sap ne et oe SS SS ee ETS 3 ;
+epee Mise = ehh Bo ae ESR, ee ee ee ee ees se oe seria
+Wie Beate: SPRSE ge Ges, SS ee ees Been yy rf po ey ta SPY Span M3 eh} Men toe Fo EE AE Ts a ce = eS NE fe ee Te ge ne te tw ety
+WS SSE ae sie Ae SS. UC eS Ee ae Ob eS Ce ear AIT S26 Ceres Tw SPER is nas Spe
+oe Bogs Se Sx BR oe atl Ro wae eee he a a “ya FN, Sei oe BP Ee NEE WETS RATES > yade yn eatco tae Sas yes}:
+Per es Se GEE SE Te conc 4 RR STE ee ae a TEER Se hoe Se age ES hee rs TERED Soh ieee
+eo Le pce SAS Makes FS ohtert eo EEISON SES Sa Mae 1 Wp PERS eg hs Tene, Sys ato wate Toyo qt bales Mee
+: Re 1 ne he at Tyee; t Bae wets was we Sa! eb mel Pek sents ne Pate eee a, =. Sore Seeiexy ea
+RS ue Sy ei fps hast ed Sa = SST AF 6 ED SG I St a 2 sy cary ;
+Pee aS aol C ; —_ Se iA ete ad's: eae ei Serpe eke Sv
+eS S33 eee SOE EAE sos , Vis eecsin bese sur ae grab wie TE ness
+pes a ky ee Pe tm ARR ABS SG EEE Seo : LE eee. g Si eae ey
+CUS aT a RES oe Geen Hs BARE a SHEER Ne sem res ee at eee Ee ee aa
+BSE te Oe Se ee sees Hi SSS ee ees es |
+eee Sey sg GMP SAEND SO a GS Or Te bases BES: SORE Se ISP OGQUESS ioc RR ee aS ba area Ma ajay Se dS at De
+goa Oe tag ee =a TAR SS VLA. RS SE Bisse: So: Siow FE Seawssee | ROR Ca Ste eet ag ta UR ise ite RS ree tas Meet fe
+rc aat tr ade aN cy 1 RESEBs Beeeti 39) Ses. inQe Ose onaee PINES iibig CS tance ead Pitts ife ies eet ing St"
+Bie ee FR SRP ST Spires aa ANG IRS REY east EQ) GER eR OS SZ KO: | . SRRRERHS 00 USSD Sse aart Bente Potala: bl ee Bae
+Bie idan SR Sey So Aes ae 2 al ae S ZB mist Eisee GS a Ge 1eSe ROS Hg PRESSE Ue Sa ee re ase ioe Nie MEP gate S|
+BA eee S| SS Sl as, SESS Sipe: joo aE Os SKE SOs RS BS Se eS ecg 8 | oe USS ee, POG Bis aed i
+oe Wig AE BeBe ia AS ae RTs eee a sai SE BP ae Sains DY x Ne fs hire en Er ge SE “
+Be he ee ea tae Rae SCALAR RBS. siaau oo; Ze S RA Peeae Pe py 3 go ee sees Boe Gee Se
+OF en SE UI Ba oh SABES ea Bset 3S Ot ai A HPeMcSRees eR . <p ial ESP haven Wait pte ty et att eeeel Ene Gan ata parc ogy
+Sabo eee! fe LAL Ss aS SES 2 ge: ES: Ai IES Raee IS NE 1G) | SS Se. peer ae Tovey atime tas ae Bey St mah TIS
+(eons i Ae Sone ot Se Shige Cheeks 1S os a ge] Sj ERE AeS eee
+ME at TS SS Ee AES eR. secs Re SR S88) Re Eak aS iS = FAM S| | TZ ZV) ce oe pce Re
+Sop Ifa gyaast ace aie SSP Sst Saarpse aie “WES SSE ROY TS oat “ae Aedsig = |He) ES eee SON DE NH xO) ON AS Mae HUES a, bn vert trcstenn Syne he yf
+Sey ae stgee” pone ee Et 1a SRST a5) Biases BAK Sek ee Bt BRAM NS Swee En) Se): 8 | Gr SS: Bee Bee S Sua ee TEE Meee 4
+BRN sate} tee Oe aah eS ae, ct Vee MEENA igh SEE MEMES BOTS ey Br OS) Hs rb ls |e Tr). es EStye. Kise ae 4 acess Lr nett yh BS
+Boo gee cae WE 3 SL e Sa; Sa oe ek & fpameieke |fe ee See SSD ge ghtwiSs Saas ene
+Fe TAN ae SS nh oa Gohan Poeuek oo ce ei 4 oy i PARR RSP EEO BELA SeOeS, BS Ss A ee Ze AYO Sipe =e Sone S Sak eae
+i a ns RES aE: wees st ~~ RS NEES Gest $Pi RE OR SSSDHD: b BS (Bxds SRE | SS H. Bees Phe SS SMAI PS ee We ey tees
+Puts iS Grae th tithe gy gfe LS, th Ne SONA fet tee SE = Reo — ie : pase | oh 4G) Berle aed os GRRL
+ash SS Aree Ree TR SEE c OA FS S45 sa GBS HE VERS ONS IS ee. KN, Bons | Sh 1 —— wnebhes Sesfont Goce ge we AE tt AN eee eee sere
+| ea As Seal OSB. ee e Bees 5. me && \ to | SEO yee ilps Se i Bae eee aie end
+Be ected Se i ee, Te “x Hee Sos Sis EARS o js RAGAN ts TSS || Samed oN (Rae SE RS BG ES Se FeSO SS
+GROIN af eee ess = SASL BE ER Bees Keay aD eo eo m |S) -. Ba fetus os “esis SERS SER alee Se
+Re RAP ee ers LEN Tin Pd As (Sei 0 SHE Ho ZOaRROEe | LESS, ee 1X: Chinn ee OT ee ee
+ate nA a SLL RO gt <= ge REO CORRE Ses Cll fn emi) >: WO PS rit eg nan nigh tote, ete LE
+}irpehoAteay apes hep ie SSS, ye Bia Biges ON GQ | es ESE wo? veh eo Ae bee nerwi e a
+1 ENE rene OS Ey SRT, oe FES: Ree : REE RVAER cpa ow SE fe = wee [ade eee ie ys Yes Sel Le SEO Sees ase}!
+it Sead sestyied stack sf ' Wee sy 2 SRS IZ FB innonUyora: UA) i ae PIECE SEEMS gS MER ED ASS TL lite at) pee ey
+Ora re ee ot cep te Ser 3 SE Dees #2 Foxes esa! é ig Beet) ly hl yeh eras avn FST et
+Bi ine Oe oe BTS We agra =sS: 8 Ne i: NRK Ones oeS: Mh n es B ae Gale here eps ergot gy,
+AS Le aan 8 No ta aR ISSO? eK Ho Mecca tain: i Me OER EE AY SSE BOM 2 ae CLE wowed MPT
+eee ae aes zt 13 BRS oe eae sso ae ce SST SS ae Spe Conca aga 71}
+t wt ea thet Soh ESF Ras Se es <2 HH  7o2e Fs rs TIBEIRS.  SRS es te ae SS >} ewe eres, § iD be ladon ee HOOT ET
+C TRS te Eo res | art Saray OO & BES: E ISAOBES So: : Gu BREU RS ees SATS ee AS ANSE TE
+TL ETMSSSS MEET Tig ey et toga inte ; a ONE SORE { aN) De NSE ATS PRs bess Atenas ARMS essy eat ied!
+Perse RE OT ne oy) bole Sr Tee REN) SST RE op et SaaS, at ae eee SNS Ss RY
+Eye = XS Puta a! WP rs e Te at wed 4 ii sa cad AB * Tea EY, *3 oe LPR E RUN pete Qe Sig Ge:
+GN ie be, «sie wh Pia hte tees A: Oe: ras essen ie ty Meelis polege “vate Hee Sot
+Te ogi tee Seep tcenn Tage ttands hubs ots utiles ee PN: BSN): ace. Suenos AF Stes SE apes ae eS SETA:
+ey ER Ne pee Ses ae Spies eta fe a, Eee SoS ge ESSER: Caen CUED Ae Shee ee AN ay TSN ce OSH Sat
+Saas Mes saps ee Ee wee She Wea BOE AMOR PAD SS en Ne ak Be PE ES de Re ene ise EOE Ee rt Ne eg ENE geen See Ne =
+EL le, ele arene Oe SSI Eh) A REIS eet Ths: SO beer ae PERE a mi sO ee ope Ot pe ot Teh Nieis See et
+<= Noh soo oe Res Rh es eh hn a Cte Sy Wie eae eee eS ae Sobre tipstes, Sa ste ear toe mgt ter Sent a Sue |
+es Rath desene san ciate saan ae Spshe eas aniees Breese eee Sree FE es ere ie ams nee cae whee sis Eee |
+. eee oe tee ese et Be ee ee oe MESS A at ae SERBS me SS oe Yet poet taser ye GP
+; : LSS SS geri ON neg Sab Se SEY:
+2 235 KOA Na en eee Loe.
+neti te ere te.
+
